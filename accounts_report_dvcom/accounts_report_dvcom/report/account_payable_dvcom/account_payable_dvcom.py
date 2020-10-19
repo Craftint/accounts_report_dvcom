eacfa2628e59d796e13a0ce9cec3b9c0be7e6f83
shifa <shifapm25@gmail.com>
2020-10-19 10:58:12 +0400

    first commit

---
+++ accounts_report_dvcom/accounts_report_dvcom/report/account_payable_dvcom/account_payable_dvcom.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+from __future__ import unicode_literals
+import frappe
+from accounts_report_dvcom.accounts_report_dvcom.report.account_receivable_dvcom.account_receivable_dvcom import ReceivablePayableReport
+
+def execute(filters=None):
+	args = {
+		"party_type": "Supplier",
+		"naming_by": ["Buying Settings", "supp_master_name"],
+	}
+	return ReceivablePayableReport(filters).run(args)
